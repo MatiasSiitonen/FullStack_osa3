3c3c62ef582db179db282debf9a2d3f3b25cfaa7
MatiasSiitonen <v.matias.siitonen@gmail.com>
2021-11-11 22:28:36 +0200

    Tehtävä 3.5

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
 
 let persons = [
     {
@@ -43,6 +45,20 @@ app.delete('/api/persons/:id', (req, res) => {
     const id = Number(req.params.id)
     persons = persons.filter(person=>person.id !==id)
     res.status(204).end()
+
+})
+
+app.post('/api/persons/', (req, res) =>{
+    const body = req.body
+
+    const newPerson = {
+        name: body.name,
+        number: body.number,
+        id: Math.floor(Math.random() * 1000)
+    }
+    persons = persons.concat(newPerson)
+    res.json(newPerson)
+
 
 })
 
