93765de688a83b10e830157e40cc8928d28313e2
MatiasSiitonen <v.matias.siitonen@gmail.com>
2021-11-11 22:49:22 +0200

    Tehtävä 3.6

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.use(express.json())
 
 let persons = [
     {
-        constent: "Arto Hellas",
+        name: "Arto Hellas",
         number: "040-123456",
         id: 1
     },
@@ -43,21 +43,41 @@ app.get('/api/persons/:id', (req, res) => {
 
 app.delete('/api/persons/:id', (req, res) => {
     const id = Number(req.params.id)
-    persons = persons.filter(person=>person.id !==id)
+    persons = persons.filter(person => person.id !== id)
     res.status(204).end()
 
 })
 
-app.post('/api/persons/', (req, res) =>{
+app.post('/api/persons/', (req, res) => {
     const body = req.body
+    const names = persons.map(person => person.name)
+
+
+
+    if (!body.number) {
+        return res.status(400).json({
+            error: 'number missing'
+        })
+    }
+    if (!body.name) {
+        return res.status(400).json({
+            error: 'name missing'
+        })
+    }
+    if (names.includes(body.name)) {
+        return res.status(400).json({
+            error: 'name must be unique'
+        })
+    }
+
 
     const newPerson = {
-        name: body.name,
-        number: body.number,
-        id: Math.floor(Math.random() * 1000)
-    }
-    persons = persons.concat(newPerson)
-    res.json(newPerson)
+    name: body.name,
+    number: body.number,
+    id: Math.floor(Math.random() * 1000)
+}
+persons = persons.concat(newPerson)
+res.json(newPerson)
 
 
 })
