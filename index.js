43156d1b1af429d34bdf748392b44a87811196c6
MatiasSiitonen <v.matias.siitonen@gmail.com>
2021-12-31 12:54:40 +0200

    Tehtävä 3.16

--- index.js
+++ index.js
@@ -12,6 +12,16 @@ app.use(cors())
 app.use(express.json())
 app.use(morgan((':method :url :status :res[content-length] - :response-time ms :body')))
 
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+  next(error)
+}
+
+
 morgan.token('body', (req, res) => {
   return [
     JSON.stringify(req.body)
@@ -36,11 +46,13 @@ app.get('/api/persons/:id', (req, res) => {
   res.send(person)
 })
 
-app.delete('/api/persons/:id', (req, res) => {
+app.delete('/api/persons/:id', (req, res, next) => {
   Person.findByIdAndDelete(req.params.id)
     .then(result => {
       res.status(204).end()
     })
+    .catch(error => next(error))
+
 })
 
 app.post('/api/persons', (request, response) => {
@@ -61,6 +73,8 @@ app.post('/api/persons', (request, response) => {
     response.json(savedPerson)
   })
 })
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
