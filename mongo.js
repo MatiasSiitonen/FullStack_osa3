8311b3c3db518183cdc513c87624e1e006a7379d
MatiasSiitonen <v.matias.siitonen@gmail.com>
2022-01-04 18:58:37 +0200

    korjaus

--- mongo.js
+++ mongo.js
@@ -1,13 +1,13 @@
 const mongoose = require('mongoose')
 
 if (process.argv.length < 3) {
-    console.log('give password as argument')
-    process.exit(1)
+  console.log('give password as argument')
+  process.exit(1)
 }
 
 if (process.argv.length === 4) {
-    console.log('give number for person')
-    process.exit(1)
+  console.log('give number for person')
+  process.exit(1)
 }
 
 const password = process.argv[2]
@@ -22,35 +22,35 @@ mongoose.connect(url)
 
 
 const personSchema = new mongoose.Schema({
-    name: String,
-    number: String,
-    date: Date,
-    important: Boolean,
+  name: String,
+  number: String,
+  date: Date,
+  important: Boolean,
 })
 
 const Person = mongoose.model('Person', personSchema)
 
 const person = new Person({
-    name: personName,
-    number: personNumber,
-    date: new Date(),
-    important: true,
+  name: personName,
+  number: personNumber,
+  date: new Date(),
+  important: true,
 })
 
 if (process.argv.length == 3) {
-    console.log('phonebook:')
-    Person.find({}).then(result => {
-        result.forEach(person => {
-            console.log(person.name, person.number)
-        })
-        mongoose.connection.close
-        process.exit(1)
+  console.log('phonebook:')
+  Person.find({}).then(result => {
+    result.forEach(person => {
+      console.log(person.name, person.number)
+    })
+    mongoose.connection.close
+    process.exit(1)
 
-    })
+  })
 } else {
-    person.save().then(response => {
-        console.log(`added ${personName} number ${personNumber} to phonebook`)
-        mongoose.connection.close()
-    })
+  person.save().then(response => {
+    console.log(`added ${personName} number ${personNumber} to phonebook`)
+    mongoose.connection.close()
+  })
 }
 
